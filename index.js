64465fb5073085f8ebd8c03631ab5a8980ddf303
Eduardo L. Costa <dev.eduardo.lc@gmail.com>
2022-10-12 20:09:04 -0300

    Configurando middlewares dos modulos de session e outros

--- index.js
+++ index.js
@@ -3,10 +3,48 @@ const exphbs = require('express-handlebars')
 const session = require('express-session')
 const flash = require('express-flash')
 const FileStore = require('session-file-store')(session)
-const conn = require('./db/conn')
+
 const { port } = require('./config')
+const conn = require('./db/conn')
 
 const app = express()
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(express.static('public'))
+
+// Session Middleware
+app.use(session({
+  name: 'session',
+  secret: 'duducmt_secret',
+  resave: false,
+  saveUninitialized: false,
+  store: new FileStore({
+    logFn: () => {},
+    path: require('path').join(require('os').tmpdir(), 'sessions'),
+  }),
+  cookie: {
+    secure: false,
+    maxAge: 360000,
+    expires: new Date(Date.now() + 360000),
+    httpOnly: true,
+  }
+}))
+
+// Flash Messages
+app.use(flash())
+
+// Salvar a session da requisição (req) na resposta (res)
+app.use((req, res, next) => {
+  if(req.session.userid) {
+    res.locals.session = req.session
+  }
+  next()
+})
 
 conn.sync()
   .then(() => {
