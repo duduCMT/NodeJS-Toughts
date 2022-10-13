da5749b5a9eb1c100596e053af046d7e876e8b64
Eduardo L. Costa <dev.eduardo.lc@gmail.com>
2022-10-13 10:24:16 -0300

    implementando logout

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -6,6 +6,11 @@ module.exports = class AuthController {
     res.render('auth/login')
   }
 
+  static logout(req, res){
+    req.session.destroy()
+    res.redirect('/login')
+  }
+
   static register(req, res){
     res.render('auth/register')
   }
