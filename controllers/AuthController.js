3183eda8f60d85708e405d17fa95f0aa4c750c6c
Eduardo L. Costa <dev.eduardo.lc@gmail.com>
2022-10-13 10:17:30 -0300

    finalizando registro de usuario e login automatico

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -10,7 +10,7 @@ module.exports = class AuthController {
     res.render('auth/register')
   }
 
-  static registerPost(req, res){
+  static async registerPost(req, res){
     const { name, email, password, confirmPassword } = req.body
 
     // Validação de senha
@@ -21,6 +21,35 @@ module.exports = class AuthController {
       return
     }
 
-    // res.redirect('auth/login')
+    // Validar se usuário existe
+    const checkIfUserExists = await User.findOne({where: {email: email}})
+
+    if(checkIfUserExists) {
+      req.flash('message', 'O email já está em uso!')
+      req.flash('messageClass', 'error')
+      res.render('auth/register')
+      return
+    }
+
+    // Criptografar senha
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password, salt)
+
+    const user = { name, email, password: hashedPassword }
+
+    User.create(user)
+      .then((createdUser) => {
+
+        // Inicializar sessão
+        req.session.userid = createdUser.id
+
+        req.flash('message', 'Cadastro realizado com sucesso!')
+        req.flash('messageClass', 'success')
+
+        req.session.save(() => {
+          res.redirect('/')
+        })
+      })
+      .catch(err => console.log(err))
   }
 }
