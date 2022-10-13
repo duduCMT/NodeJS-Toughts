6c6a67e897aa739793da5a12e0e2f9779fb144ec
Eduardo L. Costa <dev.eduardo.lc@gmail.com>
2022-10-13 09:57:56 -0300

    configurando flash messages no frontend

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
 
 module.exports = class AuthController {
   static login(req, res){
@@ -7,5 +8,19 @@ module.exports = class AuthController {
 
   static register(req, res){
     res.render('auth/register')
+  }
+
+  static registerPost(req, res){
+    const { name, email, password, confirmPassword } = req.body
+
+    // Validação de senha
+    if(password !== confirmPassword) {
+      req.flash('message', 'As senhas não conferem, tente novamente!')
+      req.flash('messageClass', 'error')
+      res.render('auth/register')
+      return
+    }
+
+    // res.redirect('auth/login')
   }
 }
