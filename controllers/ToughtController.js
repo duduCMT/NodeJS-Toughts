8e3e6f406e07240dfabba4cec6e5568a4f1d8b09
Eduardo L. Costa <dev.eduardo.lc@gmail.com>
2022-10-14 18:49:32 -0300

    finalizando criacao de pensamentos

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -13,4 +13,24 @@ module.exports = class ToughtController {
   static async createTought(req, res){
     res.render('toughts/create')
   }
+
+  static async createToughtSave(req, res) {
+    const { title } = req.body
+    const { userid } = req.session  
+
+    const tought = { title, UserId: userid }
+
+    try {
+      await Tought.create(tought)
+
+      req.flash('message', 'Pensamento criado com sucesso.')
+      req.flash('messageClass', 'success')
+      
+      req.session.save(() => {
+        res.redirect('/toughts/dashboard') 
+      })
+    } catch(err) {
+      console.log(err)
+    }
+  }
 }
--- helpers/auth.js
+++ helpers/auth.js
@@ -1,8 +1,6 @@
 module.exports.checkAuth = (req, res, next) => {
   const { userid } = req.session
 
-  console.log(req.session)
-
   if(!userid) {
     res.redirect('/login')
   }
