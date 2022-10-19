9f4aa1936194d22c82a895c869d53d455a75ef15
Eduardo L. Costa <dev.eduardo.lc@gmail.com>
2022-10-19 18:41:53 -0300

    finalizando front da home de pensamentos

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -2,8 +2,14 @@ const Tought = require('../models/Tought')
 const User = require('../models/User')
 
 module.exports = class ToughtController {
-  static showToughts(req, res){
-    res.render('toughts/home')
+  static async showToughts(req, res){
+    const toughtsData = await Tought.findAll({
+      include: User,
+    })
+
+    const toughts = toughtsData.map(result => result.get({plain: true}))
+
+    res.render('toughts/home', { toughts })
   }
   
   static async dashboard(req, res){
